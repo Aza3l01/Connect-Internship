c27225c9be4d59f037481f5ae0ea81aa325fd21e
Aza3l01 <148373215+Aza3l01@users.noreply.github.com>
2024-01-26 23:44:26 +0530

    New auth_service

---
+++ auth_service.dart
@@ -0,0 +1,62 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'firestore_service.dart';
+
+class AuthService {
+  FirebaseAuth auth = FirebaseAuth.instance;
+  final firestoreService = FirestoreService();
+  Future<void> createUser(Map<String, dynamic> data, context) async {
+    var createAt = DateTime.now().millisecond;
+    try {
+      final credential = await auth.createUserWithEmailAndPassword(
+        email: data['email'],
+        password: data['password'],
+      );
+
+      // create object
+      var userData = {
+        "id": credential.user!.uid,
+        "create": createAt,
+        "bio": "Bio",
+        "des": "Using Connect",
+        "url": "google.com",
+        "profileUrl": "https://i.imgur.com/NEJGfRn.jpg"
+      };
+
+      var userDetails = {...data, ...userData};
+
+      await firestoreService.addUser(userDetails);
+      Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: ((context) => BodyView())));
+    } catch (e) {
+      showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text("Sign Up Failed"),
+              content: Text(e.toString()),
+            );
+          });
+    }
+  }
+
+  Future<void> loginUser(data, context) async {
+    try {
+      final credential = await auth.signInWithEmailAndPassword(
+        email: data['email'],
+        password: data['password'],
+      );
+      Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: ((context) => BodyView())));
+    } catch (e) {
+      showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text("Login Failed"),
+              content: Text(e.toString()),
+            );
+          });
+    }
+  }
+}
